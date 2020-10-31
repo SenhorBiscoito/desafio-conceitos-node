37031010abd32dd865d7f0fcd3e9a80f4a0053bb
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2020-10-31 13:57:50 -0300

    Resolvendo desafio node

--- src/app.js
+++ src/app.js
@@ -1,33 +1,92 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid, v4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid project id" });
+  }
+
+  return next();
+}
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: v4(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateProjectId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(404).send("Bad Request");
+  }
+
+  const repository = { id, title, url, techs };
+
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    repository,
+  };
+
+  if (likes >= 0) {
+    return response.json({ likes: repositories[repositoryIndex].likes });
+  }
+
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).send("Bad Request");
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (!repository) {
+    return response.status(400).send("Bad Request");
+  }
+
+  repository.likes += 1;
+
+  return response.json({ likes: repository.likes });
 });
 
 module.exports = app;
